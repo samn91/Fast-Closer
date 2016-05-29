8e1b53d456363841bced9dfeb9aa84348888cd70
S a m e r <S a m e r>
2016-05-29 07:56:43 +0200

    first commit

---
+++ Nice Closer/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Nice_Closer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.killToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changeHotkeyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showHotkeyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeHotkeyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Press Here";
+            this.label4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label4_MouseDown);
+            this.label4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label4_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Process Name :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Process Diractory :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Status :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(125, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 16);
+            this.label5.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(13, 96);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 14);
+            this.label6.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(78, 50);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 16);
+            this.label7.TabIndex = 9;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.killToolStripMenuItem,
+            this.restartToolStripMenuItem,
+            this.hideToolStripMenuItem,
+            this.changeHotkeyToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(252, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // killToolStripMenuItem
+            // 
+            this.killToolStripMenuItem.Name = "killToolStripMenuItem";
+            this.killToolStripMenuItem.Size = new System.Drawing.Size(36, 20);
+            this.killToolStripMenuItem.Text = "Kill";
+            this.killToolStripMenuItem.Click += new System.EventHandler(this.killToolStripMenuItem_Click);
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.restartToolStripMenuItem.Text = "Restart";
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // hideToolStripMenuItem
+            // 
+            this.hideToolStripMenuItem.Name = "hideToolStripMenuItem";
+            this.hideToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.hideToolStripMenuItem.Text = "Hide";
+            this.hideToolStripMenuItem.Click += new System.EventHandler(this.hideToolStripMenuItem_Click);
+            // 
+            // changeHotkeyToolStripMenuItem
+            // 
+            this.changeHotkeyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showHotkeyToolStripMenuItem,
+            this.removeHotkeyToolStripMenuItem});
+            this.changeHotkeyToolStripMenuItem.Name = "changeHotkeyToolStripMenuItem";
+            this.changeHotkeyToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.changeHotkeyToolStripMenuItem.Text = "Hotkey";
+            // 
+            // showHotkeyToolStripMenuItem
+            // 
+            this.showHotkeyToolStripMenuItem.Name = "showHotkeyToolStripMenuItem";
+            this.showHotkeyToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.showHotkeyToolStripMenuItem.Text = "Show Hotkey";
+            this.showHotkeyToolStripMenuItem.Click += new System.EventHandler(this.showHotkeyToolStripMenuItem_Click);
+            // 
+            // removeHotkeyToolStripMenuItem
+            // 
+            this.removeHotkeyToolStripMenuItem.Name = "removeHotkeyToolStripMenuItem";
+            this.removeHotkeyToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.removeHotkeyToolStripMenuItem.Text = "Remove Hotkey";
+            this.removeHotkeyToolStripMenuItem.Click += new System.EventHandler(this.removeHotkeyToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(252, 189);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(258, 213);
+            this.Name = "Form1";
+            this.Text = "Closer";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem killToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changeHotkeyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hideToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showHotkeyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeHotkeyToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+    }
+}
+
---
+++ Nice Closer/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Nice_Closer
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32")]
+        public static extern bool GetCursorPos(out Point p);
+        [DllImport("user32.dll")]
+        public static extern IntPtr WindowFromPoint(Point p);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);//lpdwprocessId = IntPtr.Zero and it's return the processId  
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+
+        Process process;
+        const int ALT = 0x0001;
+        const int CTRL = 0x0002;
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void label4_MouseDown(object sender, MouseEventArgs e)
+        {
+            Cursor = Cursors.Cross;
+            AutoSize = false;
+            Size = new Size();
+        }
+
+        private void label4_MouseUp(object sender, MouseEventArgs e)
+        {
+
+            try
+            {
+                Cursor = Cursors.Default;
+                AutoSize = true;
+                process = getprocess();
+                showinfo();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Application.Restart();
+            }
+
+        }
+
+        private void showinfo()
+        {
+            label5.Text = process.ProcessName;
+            label6.Text = process.MainModule.FileName;
+            if (process.Responding)
+                label7.Text = "Responding";
+            else
+                label7.Text = "Not Responding";
+        }
+
+        Process getprocess()
+        {
+            killToolStripMenuItem.Enabled = true;
+            restartToolStripMenuItem.Enabled = true;
+            Point position;
+            GetCursorPos(out position);
+            uint fe = 0;
+            GetWindowThreadProcessId(WindowFromPoint(position), out fe);
+            return Process.GetProcessById((int)fe);
+        }
+
+        protected override void WndProc(ref Message m)//function that detect the hot key
+        {
+            try
+            {
+                if (m.Msg == 0x0312)
+                {
+
+                    if (Visible)
+                        fHide();
+                    else
+                        fShow();
+
+                }
+
+                base.WndProc(ref m);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void fShow()
+        {
+            Show();
+            process = getprocess();
+            showinfo();
+        }
+
+        private void fHide()
+        {
+            Hide();
+            process = null;
+            killToolStripMenuItem.Enabled = false;
+            restartToolStripMenuItem.Enabled = false;
+            label5.Text = "";
+            label6.Text = "";
+            label7.Text = "";
+
+        }
+       
+
+        private void killToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            process.Kill();
+            killToolStripMenuItem.Enabled = false;
+            restartToolStripMenuItem.Enabled = false;
+        }
+
+        private void hideToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fHide();
+            notifyIcon1.ShowBalloonTip(1000, "CTRL+ALT+S", "Use The Hotkey To Show/Hide The Application", ToolTipIcon.Info);
+        }
+
+      
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            killToolStripMenuItem.Enabled = false;
+            restartToolStripMenuItem.Enabled = false;
+            notifyIcon1.Text = "Click Icon To Show The Application";
+            notifyIcon1.Icon = Icon;
+            notifyIcon1.ShowBalloonTip(1000, "CTRL+ALT+S", "Use The Hotkey To Show/Hide The Application", ToolTipIcon.Info);
+            RegisterHotKey(Handle, (ALT + CTRL) ^ (int)Keys.S ^ Handle.ToInt32(), (ALT + CTRL), (int)Keys.S);
+        }
+
+        private void removeHotkeyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UnregisterHotKey(Handle, (ALT + CTRL) ^ (int)Keys.S ^ Handle.ToInt32());
+        }
+
+        private void showHotkeyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ctrl+Alt+S");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(Handle, (ALT + CTRL) ^ (int)Keys.S ^ Handle.ToInt32());
+            notifyIcon1.Visible = false;
+        }
+
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            process.Kill();
+            process = Process.Start(process.MainModule.FileName);
+        }
+
+
+    }
+}
